ae8478f5f8615a51d5d644efa1bc65ac30ac15c5
davv1d <kowalskidawid63@gmail.com>
2019-07-19 00:13:43 +0200

    Submodule 19

--- src/main/java/com/crud/tasks/domain/TaskDto.java
+++ src/main/java/com/crud/tasks/domain/TaskDto.java
@@ -2,11 +2,18 @@ package com.crud.tasks.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class TaskDto {
     private long id;
     private String title;
     private String content;
+
+    public TaskDto(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
 }
