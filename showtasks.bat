a6ac13c953b21a605ca47fe72c025d053f8b1f94
davv1d <kowalskidawid63@gmail.com>
2019-08-04 16:42:46 +0200

    Submodule 21.4

---
+++ showtasks.bat
@@ -0,0 +1,10 @@
+call runcrud.bat
+if "%ERRORLEVEL%" == "0" goto open
+echo.
+echo runcrud.bat has errors - breaking work
+goto fail
+
+:open
+sleep 5
+start "Firefox!" "C:\Program Files\Mozilla Firefox\firefox.exe" http://localhost:8080/crud/v1/task/getTasks
+
