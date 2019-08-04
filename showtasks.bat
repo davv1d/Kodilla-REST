eda5975d7dd8bef6d916acbc289ac724d5877674
davv1d <kowalskidawid63@gmail.com>
2019-08-04 16:44:59 +0200

    Submodule 21.4

--- showtasks.bat
+++ showtasks.bat
@@ -7,4 +7,12 @@ goto fail
 :open
 sleep 5
 start "Firefox!" "C:\Program Files\Mozilla Firefox\firefox.exe" http://localhost:8080/crud/v1/task/getTasks
+goto end
 
+:fail
+echo.
+echo There were errors
+
+:end
+echo.
+echo Work is finished.
