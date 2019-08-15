6d2aa23d5ad12840528b9f87b7625776dec8d196
davv1d <kowalskidawid63@gmail.com>
2019-08-15 02:45:27 +0200

    Exercise 22.4

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/v1/trello")
@@ -20,24 +19,8 @@ public class TrelloController {
     private TrelloClient trelloClient;
 
     @RequestMapping(method = RequestMethod.GET, value = "getTrelloBoards")
-    public void getTrelloBoards() {
-        Optional<List<TrelloBoardDto>> optionalTrelloBoards = trelloClient.getTrelloBoards();
-        optionalTrelloBoards.ifPresent(this::writesBoardsToConsoleWhichHaveIdAndKodillaInName);
-        optionalTrelloBoards.ifPresent(trelloBoards ->trelloBoards.forEach(trelloBoardDto -> {
-            System.out.println(trelloBoardDto.getName() + " " + trelloBoardDto.getId());
-            System.out.println("This board contains lists:");
-            trelloBoardDto.getLists().forEach(trelloList ->
-                    System.out.println(trelloList.getName() + " - " + trelloList.getId() + " " + trelloList.isClosed()));
-        }));
-    }
-
-    private void writesBoardsToConsoleWhichHaveIdAndKodillaInName(List<TrelloBoardDto> trelloBoards) {
-        trelloBoards.stream()
-                .filter(trelloBoardDto ->
-                        !trelloBoardDto.getId().isEmpty() &&
-                                !trelloBoardDto.getName().isEmpty() &&
-                                trelloBoardDto.getName().contains("Kodilla"))
-                .forEach(trelloBoardDto -> System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName()));
+    public List<TrelloBoardDto> getTrelloBoards() {
+        return trelloClient.getTrelloBoards();
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "createTrelloCard")
