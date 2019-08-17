160161678a3e7e331f164ce88ebd1598a589378d
davv1d <kowalskidawid63@gmail.com>
2019-08-18 01:49:58 +0200

    Exercise 23.4

---
+++ src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
@@ -0,0 +1,40 @@
+package com.crud.tasks.scheduler;
+
+import com.crud.tasks.config.AdminConfig;
+import com.crud.tasks.domain.Mail;
+import com.crud.tasks.repository.TaskRepository;
+import com.crud.tasks.service.SimpleEmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+import java.util.function.Function;
+
+@Component
+public class EmailScheduler {
+    private static final String SUBJECT = "Tasks: Once a day email";
+
+    @Autowired
+    private SimpleEmailService simpleEmailService;
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @Autowired
+    private AdminConfig adminConfig;
+
+    @Scheduled(cron = "0 0 10 * * *")
+    public void sendInformationEmail() {
+        long size = taskRepository.count();
+        sendEmailWithNumberOfTasks(size, numberOfTasks -> numberOfTasks == 1 ? " task." : " tasks.");
+    }
+
+    private void sendEmailWithNumberOfTasks(Long numberOfTasks, Function<Long, String> pluralOrSingular) {
+        simpleEmailService.send(new Mail(
+                adminConfig.getAdminMail(),
+                SUBJECT,
+                "Currently in database you got: " + numberOfTasks + pluralOrSingular.apply(numberOfTasks),
+                Optional.empty()));
+    }
+}
