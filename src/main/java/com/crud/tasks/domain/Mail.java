8d22f056e5ed5e2f124631fc83d7e19620b6809b
davv1d <kowalskidawid63@gmail.com>
2019-08-17 19:24:30 +0200

    Exercise 23.3

---
+++ src/main/java/com/crud/tasks/domain/Mail.java
@@ -0,0 +1,15 @@
+package com.crud.tasks.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Optional;
+
+@Getter
+@AllArgsConstructor
+public class Mail {
+    private String mailTo;
+    private String subject;
+    private String message;
+    private Optional<String> toCc;
+}
